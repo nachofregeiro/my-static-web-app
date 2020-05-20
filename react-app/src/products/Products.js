5c35d42e7806530c797f2c99d08de90dd64ff99e
Ignacio Fregeiro <nachofregeiro@gmail.com>
2020-05-20 13:33:59 -0300

    Update products title

--- react-app/src/products/Products.js
+++ react-app/src/products/Products.js
@@ -75,7 +75,7 @@ function Products({ history }) {
   return (
     <div className="content-container">
       <ListHeader
-        title="Products XXX"
+        title="Products"
         handleAdd={addNewProduct}
         handleRefresh={handleRefresh}
         routePath="/products"
